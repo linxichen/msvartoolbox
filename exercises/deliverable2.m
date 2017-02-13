e944a05b785ebc9b5926af31fce82db0590c1f72
Linxi Chen <empirechen88@gmail.com>
2017-02-13 10:51:27 -0500

    lots of plotting done for last presentation. also added exercises with ISratio, but haven't change the analysis and plotting code to use deliverable2.mat

---
+++ exercises/deliverable2.m
@@ -0,0 +1,79 @@
+%% housekeeping
+clear;
+clc;
+close all
+addpath(genpath('../helperfns'))
+
+%% generate random sequence for log name
+formatout = 'yyyy-mm-dd-HH-MM';
+st = datestr(now,formatout);
+diary(['deliverable2-',st,'.txt']);
+
+% load data
+load manual_select_dummy_FRED
+start = 0; % first start date
+shift = 0; % shift some of the variable forward or backward
+% forward_resid_rCIPI = data(start+2+shift:end,2);
+% share_rCIPI_pot = (rCIPI(2:end) - rCIPI(1:end-1))./GDPPOT(1:end-1);
+share_rCIPI_pot = share_rCIPI_potential(2:end);
+% forward_stock = data(start+1+shift:end,3);
+% match_GDP = ln_rGDP;
+match_GDP = (rGDP(2:end) - rGDP(1:end-1))./rGDP(1:end-1);
+match_sales = (rSalesGoods(2:end) - rSalesGoods(1:end-1))./rSalesGoods(1:end-1);
+match_ISratio = diff(ln_ISratio);
+data = 100*[match_sales match_GDP share_rCIPI_pot match_ISratio];
+
+% data label
+datelabel = (1949.25:0.25:2016.50)'; % because first differenced
+yearnum = floor(datelabel);
+monthnum = 12*(datelabel - yearnum)+2;
+date_serial = datenum(yearnum,monthnum,ones(size(yearnum)));
+break_dummy = (datelabel >= 1984.00);
+
+%% load regimes from MLE
+load regimes_MLE regimes_mat
+
+%% specify the empirical model
+model.p = 3;
+model.T = length(regimes_mat);
+model.breakdate = find(datelabel == 1984.00,1,'first');
+condition.regimes = regimes_mat;
+condition.y_table = data;
+
+%% specify priors for hyperpara, very flat
+p = model.p;
+N = size(data,2); model.N = N;
+M1 = max(regimes_mat(:,1)); model.M1 = M1;
+M2 = max(regimes_mat(:,2)); model.M2 = M2;
+priors.mmu_mean = zeros(N*2*M1,1);
+priors.mmu_cov = 1e5*eye(N*2*M1);
+priors.pphi_mean = zeros(N*N*p*M1,1);
+priors.pphi_cov = 1e5*eye(N*N*p*M1);
+priors.Ssigma_SSR = 0;
+priors.Ssigma_nnu = 0;
+
+%% specify options for the Gibbs sampler
+options.burnin = 2e4;
+options.R = 5e4;
+options.pphi_init = zeros(p*N*N*M1,1);
+options.mmu_init = zeros(N*2*M1,1);
+for i_regime2 = 1:M2
+	options.Ssigma_array_init(:,:,i_regime2) = eye(N);
+end
+
+tic
+draws = dummyVAR_Gibbs(data,regimes_mat,model,priors,options);
+toc
+
+%% look at result
+median(draws.Ssigma_array,4)
+mean(draws.pphi,2)
+mean(draws.mmu,2)
+
+save deliverable2.mat
+diary off
+
+%% 
+RF_analysis;
+RF_analysis_plots;
+
--- helperfns/RF_analysis_plots.m
+++ helperfns/RF_analysis_plots.m
@@ -6,47 +6,137 @@ addpath(genpath('../helperfns'))
 load('deliverable1_analysis.mat')
 
 %% Create tables and graphs after analysis
-%% Get FEVD tables first
-prctile(squeeze(corr_array(3,1,1,1,:)),16)
-prctile(squeeze(corr_array(3,1,1,1,:)),50)
-prctile(squeeze(corr_array(3,1,1,1,:)),84)
+% Get the steady state numbers
+mmu_array = reshape(draws.mmu,[N 2 M1 R]);
+%%
+ar_regime = 1;
+sb_period = 1;
+for i_var = 1:N
+	i_var
+	prctile(squeeze(mmu_array(i_var,sb_period,ar_regime,:)),16)
+	prctile(squeeze(mmu_array(i_var,sb_period,ar_regime,:)),50)
+	prctile(squeeze(mmu_array(i_var,sb_period,ar_regime,:)),84)
+	disp('==========')
+end
 
-prctile(squeeze(corr_array(3,1,2,1,:)),16)
-prctile(squeeze(corr_array(3,1,2,1,:)),50)
-prctile(squeeze(corr_array(3,1,2,1,:)),84)
+% Get the steady state numbers
+ar_regime = 2;
+sb_period = 1;
+for i_var = 1:N
+	i_var
+	prctile(squeeze(mmu_array(i_var,sb_period,ar_regime,:)),16)
+	prctile(squeeze(mmu_array(i_var,sb_period,ar_regime,:)),50)
+	prctile(squeeze(mmu_array(i_var,sb_period,ar_regime,:)),84)
+	disp('==========')
+end
 
-prctile(squeeze(corr_array(2,3,1,2,:)),16)
-prctile(squeeze(corr_array(2,3,1,2,:)),50)
-prctile(squeeze(corr_array(2,3,1,2,:)),84)
+%% Get FEVD tables first, low AR and low COV regimes
+ar_regime = 1;
+cov_regime = 1;
 
-prctile(squeeze(corr_array(2,3,2,2,:)),16)
-prctile(squeeze(corr_array(2,3,2,2,:)),50)
-prctile(squeeze(corr_array(2,3,2,2,:)),84)
+table_16 = zeros(N,N);
+table_50 = zeros(N,N);
+table_84 = zeros(N,N);
+for i = 1:N
+	for j = 1:N
+		table_16(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),16);
+		table_50(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),50);
+		table_84(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),84);
+	end
+end
 
-prctile(squeeze(corr_array(2,1,1,2,:)),16)
-prctile(squeeze(corr_array(2,1,1,2,:)),84)
+%% Get FEVD tables first, low AR and low COV regimes
+ar_regime = 2;
+cov_regime = 1;
 
-prctile(squeeze(corr_array(2,1,2,2,:)),16)
-prctile(squeeze(corr_array(2,1,2,2,:)),84)
+table_16 = zeros(N,N);
+table_50 = zeros(N,N);
+table_84 = zeros(N,N);
+for i = 1:N
+	for j = 1:N
+		table_16(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),16);
+		table_50(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),50);
+		table_84(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),84);
+	end
+end
 
-prctile(squeeze(cov_array(3,3,1,1,:)),16)
-prctile(squeeze(cov_array(3,3,1,1,:)),84)
+%% Get FEVD tables first, low AR and low COV regimes
+ar_regime = 1;
+cov_regime = 2;
 
-prctile(squeeze(cov_array(3,3,2,1,:)),16)
-prctile(squeeze(cov_array(3,3,2,1,:)),84)
+table_16 = zeros(N,N);
+table_50 = zeros(N,N);
+table_84 = zeros(N,N);
+for i = 1:N
+	for j = 1:N
+		table_16(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),16);
+		table_50(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),50);
+		table_84(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),84);
+	end
+end
+
+%% Get FEVD tables first, low AR and low COV regimes
+ar_regime = 2;
+cov_regime = 2;
+
+table_16 = zeros(N,N);
+table_50 = zeros(N,N);
+table_84 = zeros(N,N);
+for i = 1:N
+	for j = 1:N
+		table_16(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),16);
+		table_50(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),50);
+		table_84(i,j) = prctile(squeeze(corr_array(i,j,ar_regime,cov_regime,:)),84);
+	end
+end
+
+%% serious IRF plotting
+width = 8;     % Width in inches
+height = 5;    % Height in inches
+alw = 1.1;    % AxesLineWidth
+fsz = 11;      % Fontsize
+lw = 1.5;      % LineWidth
+msz = 8;       % MarkerSize
+
+h = figure('Units','inches',...
+	'Position',[0 0 width height]);
+
+plot_horizon = 15;
+subplot(1,2,1)
+plot(0:plot_horizon,prctile(squeeze(OIRF_array(1,3,1:plot_horizon+1,1,1,:)),50,2),'b-',...
+	0:plot_horizon,prctile(squeeze(OIRF_array(1,3,1:plot_horizon+1,2,1,:)),50,2),'r-.','LineWidth',lw,'MarkerSize',msz ...
+	)
+legend('Expansion','Recession')
+xlabel('Periods From Imparct','FontSize',10)
+ylabel('Percentage')
+ylabh = get(gca,'yLabel');
+set(ylabh,'Position',get(ylabh,'Position') - [2 0 0])
+title('Inventory Investment')
+
+subplot(1,2,2)
+plot(0:plot_horizon,prctile(squeeze(OIRF_array(1,2,1:plot_horizon+1,1,1,:)),50,2),'b-',...
+	0:plot_horizon,prctile(squeeze(OIRF_array(1,2,1:plot_horizon+1,2,1,:)),50,2),'r-.','LineWidth',lw,'MarkerSize',msz...
+	)
+xlabel('Periods From Imparct')
+title('GDP Growth')
+legend('Expansion','Recession')
+print('OIRF_sales','-depsc2','-r300');
+
 
 %% plotting
 figure
 subplot(2,1,1)
-plot(0:horizon,prctile(squeeze(OIRF_array(2,1,:,1,1,:)),16,2),...
-	0:horizon,prctile(squeeze(OIRF_array(2,1,:,1,1,:)),50,2),...
-	0:horizon,prctile(squeeze(OIRF_array(2,1,:,1,1,:)),84,2)...
+plot(0:horizon,prctile(squeeze(OIRF_array(1,3,:,1,1,:)),16,2),...
+	0:horizon,prctile(squeeze(OIRF_array(1,3,:,1,1,:)),50,2),...
+	0:horizon,prctile(squeeze(OIRF_array(1,3,:,1,1,:)),84,2)...
 	)
 subplot(2,1,2)
-plot(0:horizon,prctile(squeeze(OIRF_array(2,1,:,2,1,:)),16,2),...
-	0:horizon,prctile(squeeze(OIRF_array(2,1,:,2,1,:)),50,2),...
-	0:horizon,prctile(squeeze(OIRF_array(2,1,:,2,1,:)),84,2)...
+plot(0:horizon,prctile(squeeze(OIRF_array(1,3,:,2,1,:)),16,2),...
+	0:horizon,prctile(squeeze(OIRF_array(1,3,:,2,1,:)),50,2),...
+	0:horizon,prctile(squeeze(OIRF_array(1,3,:,2,1,:)),84,2)...
 	)
+
+
 
 figure
 subplot(2,1,1)
